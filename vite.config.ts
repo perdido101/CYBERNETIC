1a4059b60f62aaaa70a8eacda298b0490d5fdf79
perdido101 <98296653+perdido101@users.noreply.github.com>
2025-01-10 21:46:23 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/neural-content-synthesizer/', // Replace with your repo name
+  base: '/CYBERNETIC/', // Update to match your repo name exactly
   plugins: [react()],
   server: {
     proxy: {
